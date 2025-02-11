9eca05cf9715dbaf35390596474cf71ab1ee4e32
Imanol Manrique Lluque <88408719+lonami10@users.noreply.github.com>
2025-02-10 23:29:14 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+// Puedes agregar interactividad aquí si lo deseas, como un carrusel de promociones o un formulario de contacto.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página cargada y lista para usar.');
+});
