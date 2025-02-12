a1c3ab6dd098d56abea1bd1751cb90ef4a58771b
Imanol Manrique Lluque <88408719+lonami10@users.noreply.github.com>
2025-02-11 23:48:34 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Página cargada y lista para usar.');
 });
+
+
+function agrandarImagen(imagen) {
+    // Cierra todas las imágenes agrandadas primero
+    const imagenesAgrandadas = document.querySelectorAll('.agrandada');
+    imagenesAgrandadas.forEach(img => {
+        if (img !== imagen) {
+            img.classList.remove('agrandada');
+        }
+    });
+
+    // Alterna la clase 'agrandada' en la imagen clicada
+    imagen.classList.toggle('agrandada');
+}
+
+// Cierra la imagen agrandada al hacer clic fuera de ella
+document.addEventListener('click', function(event) {
+    const imagenesAgrandadas = document.querySelectorAll('.agrandada');
+    if (imagenesAgrandadas.length > 0 && !event.target.classList.contains('promo-image')) {
+        imagenesAgrandadas.forEach(img => {
+            img.classList.remove('agrandada');
+        });
+    }
+});
